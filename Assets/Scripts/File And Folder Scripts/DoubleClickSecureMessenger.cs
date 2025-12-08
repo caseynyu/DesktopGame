3e8e9fddfb35fe8c72073fc114ffef1a1160d2c5
tochgl <40646750+tochgl@users.noreply.github.com>
2025-12-08 14:23:34 -0500

    added messenger desktop icons and dowload secure messenger

---
+++ Assets/Scripts/File And Folder Scripts/DoubleClickSecureMessenger.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+public class DoubleClickSecureMessenger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    bool hovering = false;
+    bool clickedOnce = false;
+    public GameObject attachedWindow;
+    [SerializeField]
+    private float doubleClickTimeMax=.4f;
+    private float doubleClickTimeCount;
+
+    void Start()
+    {
+        attachedWindow = FindFirstObjectByType<SecureMessengerTextBox>(FindObjectsInactive.Include).GetComponentInParent<WebsiteSwitcher>(true).gameObject;
+        doubleClickTimeCount = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(clickedOnce == true)
+        {
+            doubleClickTimeCount+=Time.deltaTime;
+            if (doubleClickTimeCount >= doubleClickTimeMax)
+            {
+                doubleClickTimeCount = 0;
+                clickedOnce = false;
+            }
+        }
+        else
+        {
+            doubleClickTimeCount = 0;
+        }
+
+        if (hovering && Input.GetMouseButtonDown(0))
+        {
+            if (clickedOnce)
+            {
+                if (attachedWindow.activeSelf)
+                {
+                    attachedWindow.SetActive(false);
+                }
+                else
+                {
+                    attachedWindow.SetActive(true);
+                }
+                
+                attachedWindow.GetComponent<RectTransform>().SetAsLastSibling();
+            }
+            else
+            {
+                clickedOnce = true;
+                //StartCoroutine(doubleClick());
+            }
+        }
+
+    }
+
+    public void PointerEnter()
+    {
+        hovering = true;
+    }
+    public void PointerExit()
+    {
+        hovering = false;
+    }
+}
